63361a8e61bc37b560320a36afb2aee1f63e12de
Brant Goings <bgraulph@gmail.com>
2020-02-18 13:46:56 -0500

    Expand comment

--- LBD Hash Cracking/SimpleCracker.py
+++ LBD Hash Cracking/SimpleCracker.py
@@ -20,7 +20,7 @@ def crack():
                     # Starts nested for loop in hashreader
                     for hashline in hashreader:
 
-# -------------------- BEGIN SUPER IMPORTANT PART -------------------- #
+# ------------------------------------------------------------ BEGIN SUPER IMPORTANT PART ------------------------------------------------------------ #
                         # If statement compares the hashed version of the password in dictionary.txt with the hash in hashes.txt
                         if hashlib.md5(dictline.rstrip().encode()).hexdigest() == hashline.rstrip():
                             # If they match, print the hash and the line in dictionary.txt we're currently at
@@ -28,7 +28,7 @@ def crack():
                         # If they're not the same, continue without doing anything
                         else:
                             pass
-# -------------------- END SUPER IMPORTANT PART -------------------- #
+# ------------------------------------------------------------ END SUPER IMPORTANT PART ------------------------------------------------------------ #
 
         except:
             pass
